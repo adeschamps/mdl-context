6d761f819793f6cf284324016d6de18eba916db2
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-11-25 09:28:28 -0500

    Add documentation from readme.

--- src/Context.elm
+++ src/Context.elm
@@ -7,6 +7,27 @@ module Context
         , child
         )
 
+{-|
+A collection of helpers for dealing with elm-mdl render functions.
+This is motivated by the following observations:
+
+- The triplet of `Mdl index model.mdl` occurs frequently in elm-mdl.
+- Dealing with indices is a bit tedious.
+
+    viewButton model index =
+        Button.render Mdl (0 :: index) model.mdl
+            [ Button.onClick Increment ]
+            [ text "Add" ]
+
+This becomes
+
+    viewButton context model =
+        (Button.render |> with context)
+        Button.render Mdl (0 :: index) model.mdl
+            [ Button.onClick Increment ]
+            [ text "Add" ]
+-}
+
 import Parts
 
 
